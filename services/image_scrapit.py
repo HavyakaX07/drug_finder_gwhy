34403270cf7692268ddbc53151dd80327426ea59
HEGDE <prasanna.hegde@siemens.com>
2022-09-28 00:43:22 +0530

    no change

--- services/image_scrapit.py
+++ services/image_scrapit.py
@@ -8,15 +8,16 @@ import os
 from selenium.webdriver.chrome.service import Service
 
 chrome_options=webdriver.ChromeOptions()
-chrome_options.binary_location=os.environ.get("GOOGLE_CHROME_BIN")
+chrome_options.binary_location=os.environ.get("GOOGLE_gitCHROME_BIN")
 chrome_options.add_argument("--headless")
 chrome_options.add_argument("--disable-dev-shm-usage")
 chrome_options.add_argument("--no-sandbox")
 
 def fetch_drug_image(drug_info:str,drug_dict):
+    print("URl",drug_info)
     if drug_info != "None":
         try:
-            #driver = webdriver.Chrome(ChromeDriverManager().install())
+            # driver = webdriver.Chrome(ChromeDriverManager().install())
             driver = webdriver.Chrome(service=Service(executable_path=os.environ.get("CHROMEDRIVER_PATH")), options=chrome_options)
             driver.get(drug_info)
             #stabalise
